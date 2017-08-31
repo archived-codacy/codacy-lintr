a40526f122011735d6a134767196f58589801929
Eddie Pantridge <erp12@hampshire.edu>
2017-08-31 14:41:50 -0400

    start R linting (#4)

--- src/codacy-lintr.R
+++ src/codacy-lintr.R
@@ -1,15 +1,23 @@
-args <- commandArgs(trailingOnly = TRUE)
-print(args[1])
+setwd('~/Documents/General/data_days/summer_2017/codacy-lintr/')
+library(jsonlite)
 
+args_json <- fromJSON('codacy-test.json')
+# args_json <- args[1]
 
+# names(args_json)
+# args_json$source
+# args_json$configuration
+# args_json$files
+# args_json$options
+# args_json$specification
 
-
-
-# Only .R files should be passed to this function
-lint_files <- function(files) {
-  violations <- list()
-  for (f in files) {
-    violations <- c(violations, lintr::lint(f))
+for(file in args_json$files){
+  # file = args_json$files[1]
+  print(file)
+  ext = toupper(strsplit(file, '\\.')[[1]][-1])
+  if(ext=="R"){
+    print('   linting...')
+    lintr::lint(file)
   }
-  violations
 }
+>>>>>>> fbd8cb4624fed2c16a7b2c48223b12bc048a9c83
